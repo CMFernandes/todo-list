f0fb2a0bb7bb6fc718fafbdd19038ce31bd6ae19
cmfernandes <cristianafmfernandes@gmail.com>
2023-10-20 19:38:37 +0100

    moved addTop func to eventHandler.js add handleAddTask func & displayTaskInput

--- src/displayUi.js
+++ src/displayUi.js
@@ -1,25 +1,9 @@
 import { TopicList } from "./topicList";
-import { Topic } from "./topic";
-import { Task } from "./task";
-import { handleTopicsBtn, delTopic } from "./eventsHandler";
-
+import { handleTopicsBtn, delTopic, handleAddTasks, addTop } from "./eventsHandler";
 
 export const list = new TopicList();
 
 
-function addTop(){
-    let titleInput = document.getElementById("topic-title");
-    const title = titleInput.value;
-    if(title === ""){
-        displayMessage("The topic title can't be empty!");
-        return;
-    }
-    list.addTopic(title);
-    
-    displayTopicsBtn(title);
-
-    titleInput.value = "";
-}
 export function initialize() {
     const inBoxBtn = document.querySelector('[data-name="Inbox"]');
     inBoxBtn.addEventListener('click', handleTopicsBtn);
@@ -28,7 +12,7 @@ export function initialize() {
     addTopicBtn.addEventListener("click", addTop);
 }
 
-function displayTopicsBtn(title){
+export function displayTopicsBtn(title){
     let topicName = title;
 
     const topicListNav = document.querySelector(".nav-topics");
@@ -68,25 +52,28 @@ function createTopicCont(topic){
     topicTitle.textContent = topic.title;
 
     const topicBodyCont = createElementWithAttribute("div","class","topic-body-cont")
-    
-    const addTaskDiv = document.createElement("div")
-    const addTask = createElementWithAttribute("i","class","fa fa-plus")
-    const addTaskText = createElementWithAttribute("span", "class", "add-task-text");
-    addTaskText.textContent = "Add Task";
+    const taskCont = createElementWithAttribute("div","class", "task-cont");
+
+    const displayTaskDiv = createElementWithAttribute("div","class","display-task-div")
+    const displayTaskBtnCont = document.createElement("div")
+    const displayTaskBtn = createElementWithAttribute("i","class","fa fa-plus")
+    const displayTaskText = createElementWithAttribute("span", "class", "add-task-text");
+    displayTaskText.textContent = "Add Task";
     
 
     todoCont.innerHTML = '';
 
-    
     todoCont.appendChild(topicHeaderContainer);
     todoCont.appendChild(topicBodyCont);
-    addTaskDiv.appendChild(addTask);
-    addTaskDiv.appendChild(addTaskText);
+    displayTaskDiv.appendChild(displayTaskBtnCont)
+    displayTaskBtnCont.appendChild(displayTaskBtn);
+    displayTaskBtnCont.appendChild(displayTaskText);
     topicHeaderContainer.appendChild(topicTitle);
-    topicHeaderContainer.appendChild(addTaskDiv);
-    
-}
+    topicHeaderContainer.appendChild(displayTaskDiv);
+    topicBodyCont.appendChild(taskCont)
 
+    displayTaskBtnCont.addEventListener('click', displayTaskInput)
+}
 
 
 export function removeTopicUI(dataName){
@@ -98,4 +85,37 @@ function createElementWithAttribute(elementType,attribute, attributeName){
     const element = document.createElement(elementType);
     element.setAttribute(attribute, attributeName);
     return element;
+}
+
+export function displayTaskInput(){
+    const displayTaskDiv = document.querySelector(".display-task-div")
+    const taskInputCont = createElementWithAttribute("div","class","task-input-cont");
+    
+    const taskTitleInput = createElementWithAttribute("input","id","task-title-input")
+    taskTitleInput.type = "text";
+    taskTitleInput.setAttribute("placeholder","Enter a title")
+
+    const taskDescripInput = createElementWithAttribute("input","id","task-descrip-input")
+    taskDescripInput.type = "text";
+    taskDescripInput.setAttribute("placeholder","Enter a description")
+
+    const addTaskBtn = createElementWithAttribute("i","class","fa fa-plus")
+
+    displayTaskDiv.appendChild(taskInputCont)
+    taskInputCont.appendChild(taskTitleInput)
+    taskInputCont.appendChild(taskDescripInput)
+    taskInputCont.appendChild(addTaskBtn)
+
+    addTaskBtn.addEventListener('click', handleAddTasks)
+}
+
+export function displayTask(title, descrip){
+    const taskCont = document.querySelector(".task-cont")
+    const taskDiv = createElementWithAttribute("div", "class", "task-div");
+
+    const checkbox = createElementWithAttribute("input", "class", "checkbox")
+    checkbox.type = "checkbox";
+
+    taskCont.appendChild(taskDiv)
+    taskDiv.appendChild(checkbox)
 }
--- src/eventsHandler.js
+++ src/eventsHandler.js
@@ -1,12 +1,26 @@
-import { list } from "./displayUi";
-import { displayTopic } from "./displayUi";
-import { removeTopicUI } from "./displayUi";
+import { list, displayTopic, displayTopicsBtn, removeTopicUI, displayTask} from "./displayUi";
+
 
 export function handleTopicsBtn(e){
     const listItems = list.getTopicList();
     const dataName = e.target.getAttribute("data-name");
     const topic = listItems.find(topic => topic.title === dataName)
     displayTopic(topic)
+}
+
+export function addTop(){
+    let titleInput = document.getElementById("topic-title");
+    const title = titleInput.value;
+    
+    if(title === ""){
+        displayMessage("The topic title can't be empty!");
+        return;
+    }
+    list.addTopic(title);
+    
+    displayTopicsBtn(title);
+
+    titleInput.value = "";
 }
 
 export function delTopic(e){
@@ -16,3 +30,12 @@ export function delTopic(e){
     removeTopicUI(dataName)
 }
 
+export function handleAddTasks(){
+    const listItems = list.getTopicList();
+    const dataName = document.querySelector(".topic-title").textContent;
+    const topic = listItems.find(topic => topic.title === dataName);
+    const title = document.getElementById("task-title-input").value
+    const descrip = document.getElementById("task-descrip-input").value;
+    topic.addTask(title, descrip)
+    displayTask(title, descrip)
+}
